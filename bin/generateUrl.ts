b536d994497a5f4a9acbaec24f78437aa13f03a6
inajung <ina.jung@coint.co.kr>
2023-12-07 14:13:14 +0900

    refactor: 저장 경로 수정

--- bin/generateUrl.ts
+++ bin/generateUrl.ts
@@ -14,13 +14,12 @@ type Page = {
 type Dir = {
   kind: "dir";
   readonly name: string;
-  readonly children: Array<Page | Dir>;
+  readonly children: (Page | Dir)[];
 };
 
 function parseSource(parentDir: string): Array<Page | Dir> {
   const entries = fs.readdirSync(parentDir, { withFileTypes: true });
-  // eslint-disable-next-line no-array-constructor
-  const contents = Array<Page | Dir>();
+  const contents: (Page | Dir)[] = [];
 
   for (let i = 0; i < entries.length; i++) {
     const entry = entries[i];
@@ -58,8 +57,7 @@ function generateSources(
 }
 
 function generateSource(page: Page | Dir, parents: string[]): string[] {
-  // eslint-disable-next-line no-array-constructor
-  const lines = Array<string>();
+  const lines: string[] = [];
   const newParents = [...parents, page.name === "index" ? "" : page.name];
   switch (page.kind) {
     case "page": {
@@ -86,11 +84,10 @@ function generateSource(page: Page | Dir, parents: string[]): string[] {
   return lines;
 }
 
-const pagesDir = path.join(__dirname, "..", "src/pages");
+const pagesDir = path.join(__dirname, "..", "src/app");
 const pages = parseSource(pagesDir);
 
-// eslint-disable-next-line no-array-constructor
-const ts = Array<string>();
+const ts: string[] = [];
 ts.push("/* tslint:disable */");
 ts.push("/* eslint-disable */");
 ts.push(`// 자동 생성 파일 수정하지 말것 ${new Date().toString()}`);
