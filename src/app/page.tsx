92e713ab5ae9dc688a07c446bb545f5f33f6175b
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-11 21:21:20 +0900

    refactor: useDarkMode hook 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Urls } from "@/url/url.g";
+import useDarkMode from "@/hook/useDarkMode";
 
 export default function Home() {
-  const localStorageCheker = (): boolean => {
-    if (!localStorage.theme) return false;
-    return localStorage.theme === "dark";
-  };
-  const [dark, setDark] = useState(localStorageCheker());
-  const darkSetButton = () => {
-    setDark((state) => {
-      const update = !state;
-      if (update) {
-        localStorage.theme = "dark";
-      } else {
-        localStorage.theme = "light";
-      }
-      return update;
-    });
-  };
-
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [dark]);
+  const { onClickToggleButton } = useDarkMode();
 
   return (
     <main className="">
       <p className="text-[100px] font-light">지마켓</p>
-      <button type="button" onClick={() => darkSetButton()}>
+      <button type="button" onClick={() => onClickToggleButton()}>
         toggle
       </button>
       <Link href={Urls.about.index.url()}>about</Link>
