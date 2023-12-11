4fad5285329850675892c1e8d80963ec8ed70192
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-11 21:22:27 +0900

    refactor: eslint

--- src/hook/useDarkMode.ts
+++ src/hook/useDarkMode.ts
@@ -19,8 +19,7 @@ const useDarkMode = (): {
   useEffect(() => {
     if (
       localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
     } else {
