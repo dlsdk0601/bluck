880498280f046d53212e14810d5df85a4cc55299
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-14 01:29:26 +0900

    refactor: Footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@
 
 import { PropsWithChildren, ReactNode } from "react";
 import { RecoilRoot } from "recoil";
-import Header from "@/view/layout/Header";
+import HeaderView from "@/view/layout/HeaderView";
 import { roboto } from "@/view/layout/fonts";
 import "./globals.css";
+import FooterView from "@/view/layout/Footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -18,11 +19,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
 const ClientLayout = (props: PropsWithChildren) => {
   return (
-    <body className={`${roboto.className} gmarket bg-ccfd1dd py-11 dark:bg-black`}>
+    <body className={`${roboto.className} gmarket bg-ccfd1dd py-8 dark:bg-black`}>
       <section className="bg-cffffff dark:bg-cFFFFFF4C mx-auto my-0 h-[89vh] w-[96vw] overflow-hidden rounded-3xl">
-        <Header />
+        <HeaderView />
         <main className="h-[100vh] w-full px-10">{props.children}</main>
       </section>
+      <FooterView />
     </body>
   );
 };
