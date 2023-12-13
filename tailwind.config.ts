70341803039aa7ec7fa01bb50882646d36417992
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-13 23:18:54 +0900

    refactor: media query 사이즈 추가 및 color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,47 @@ const config: Config = {
   darkMode: "class",
   content: ["./src/view/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    screens: {
+      tablet: { max: "1024px" },
+      mobile: { max: "768px" },
+    },
     fontFamily: {
       gmarket: ["var(--font-gmarket)", "sans-serif"],
     },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        loader: "loader 1s ease-in infinite",
       },
+      keyframes: {
+        loader: {
+          "0%": {
+            transform: "scale(0)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "0",
+          },
+        },
+      },
+    },
+    colors: {
+      c1f295a: "#1f295a", // navText
+      ccfd1dd: "#cfd1dd", // navBgColor
+      cffffff: "#ffffff", // noticeColor
+      cf9f9f9: "#f9f9f9", // ctBgColor
+      c4c547b: "#4c547b", // menuBgColor
+      cedeff6: "#edeff6", // input
+      cFFFFFF4C: "#FFFFFF4C", // dark_noticeColor
+      c000000: "#000000", // dark_bgColor
+      c000000CC: "#000000CC", // dark_ctBgColor
+      // c1f295a: "#1f295a", // mainText
+      // ccfd1dd: "#cfd1dd", // bgColor
+      // cffffff: "#ffffff", // dark_navText
+      // c1f295a: "#1f295a", // dark_navBgColor
+      // cffffff: "#ffffff", // dark_mainText
+      // c4c547b: "#4c547b", // dark_menuBgColor
+      // cedeff6: "#edeff6", // dark_input
     },
   },
   plugins: [],
