212c5daf7a8a1d1558597c70c71fcda7966c3a7a
inajung <ina.jung@coint.co.kr>
2023-12-19 16:39:30 +0900

    feat: Link 와 push 가 더이상 UrlObject 를 지원하지 않아서 string 을 return 해주는 메서드 추가

--- src/url/url.ts
+++ src/url/url.ts
@@ -1,5 +1,7 @@
 import { UrlObject } from "url";
 import { ParsedUrlQueryInput } from "querystring";
+import { head, isArray, isNil } from "lodash";
+import { removeSuffix } from "@/ex/utils";
 
 export class PageUrl {
   readonly pathname: string;
@@ -10,5 +12,50 @@ export class PageUrl {
 
   url(query?: ParsedUrlQueryInput): UrlObject {
     return { pathname: this.pathname, query };
+  }
+
+  urlString(query?: ParsedUrlQueryInput) {
+    if (isNil(query)) {
+      return this.pathname;
+    }
+
+    const queryArr = Object.keys(query ?? {}).map((key) => {
+      const value = this.codecQueryValue(query[key]);
+      return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+    });
+    const queryString = queryArr.join("&");
+
+    return `${this.pathname}?${queryString}`;
+  }
+
+  urlPk(query: { pk: number }) {
+    const pureUrl = removeSuffix(this.pathname, "pk");
+
+    return `${pureUrl}${query.pk}`;
+  }
+
+  codecQueryValue(
+    value:
+      | string
+      | number
+      | boolean
+      | readonly string[]
+      | readonly number[]
+      | readonly boolean[]
+      | null
+      | undefined,
+  ) {
+    if (isNil(value)) {
+      return "";
+    }
+
+    if (isArray(value)) {
+      // 굳이 복사 하는 이유는 ts 가 타입 추적을 못함..
+      const newValue: readonly string[] | readonly number[] | readonly boolean[] = [...value];
+      const arr = newValue.map((item) => item.toString());
+      return head(arr)?.toString() ?? "";
+    }
+
+    return value.toString();
   }
 }
