619ae7b9d2832ecd1c3c182803c054555314bce6
inajung <ina.jung@coint.co.kr>
2023-12-19 16:55:24 +0900

    refactor: 불필요한 로직 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import { Urls } from "@/url/url.g";
-import useDarkMode from "@/hook/useDarkMode";
-
 export default function Home() {
-  const { onClickToggleButton } = useDarkMode();
-
   return (
     <main className="">
       <p className="text-[100px] font-light">지마켓</p>
-      <button type="button" onClick={() => onClickToggleButton()}>
-        toggle
-      </button>
-      <Link href={Urls.about.index.url()}>about</Link>
     </main>
   );
 }
