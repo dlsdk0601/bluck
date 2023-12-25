383c9d2318e6f299f7fe437554ff58baecb4f7f9
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-26 03:08:30 +0900

    feat: 검색 조건 및 searchBox 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import { MainSelectBoxView } from "@/view/SelectBoxView";
+import SearchBox from "@/view/SearchBox";
+
 export default function Home() {
   return (
-    <main className="">
-      <p className="text-[100px] font-light">지마켓</p>
-    </main>
+    <div className="h-full w-full">
+      <div className="flex items-center justify-between mobile:flex-wrap">
+        {/*  menu */}
+        <div className="flex items-center justify-between">
+          <MainSelectBoxView
+            value="A"
+            options={[
+              ["A", "인기순"],
+              ["B", "최신순"],
+              ["C", "조회수"],
+            ]}
+          />
+          <MainSelectBoxView
+            value="D"
+            options={[
+              ["D", "오늘"],
+              ["E", "이번주"],
+              ["F", "이번달"],
+              ["G", "올해"],
+            ]}
+          />
+          <SearchBox />
+        </div>
+      </div>
+      <div className="height-[67vh] mt-[10px] flex w-full flex-wrap justify-between overflow-y-auto pr-[1%] tablet:h-[72vh]">
+        {/*  content */}
+      </div>
+    </div>
   );
 }
