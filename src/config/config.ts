89fb445ed61debea371874834c4ec86e7ce621a0
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-30 01:02:05 +0900

    feat: dev 환경 변수 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 class Config {
   readonly baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
+  readonly isDev = process.env.NEXT_PUBLIC_IS_DEV === "true";
 }
 
 export const config = new Config();
