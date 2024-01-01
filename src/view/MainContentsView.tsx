e750d707f407e69075dec7843117a3b998b756f2
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-01 23:47:59 +0900

    feat: faker fetch 함수 반영

--- src/view/MainContentsView.tsx
+++ src/view/MainContentsView.tsx
@@ -1,12 +1,12 @@
-"use client";
-
-import { range } from "lodash";
 import ContentsCardView from "@/view/ContentsCardView";
+import { fetchMainContents } from "@/server/data";
 
-const MainContentsView = () => {
+const MainContentsView = async (props: { searchType?: string; searchDateType?: string }) => {
+  const contents = await fetchMainContents();
+
   return (
     <div className="mt-[10px] flex h-[70vh] w-full flex-wrap justify-between overflow-y-auto pr-[1%] tablet:h-[72vh]">
-      {range(0, 15).map((item) => (
+      {contents.map((item) => (
         <ContentsCardView isFull={false} />
       ))}
     </div>
