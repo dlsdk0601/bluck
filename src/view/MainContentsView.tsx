bd50d0d8f421e12e44514cd8b70573fb9767e5ef
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-02 23:55:00 +0900

    feat: warning 이 나서 key 추가

--- src/view/MainContentsView.tsx
+++ src/view/MainContentsView.tsx
@@ -2,12 +2,12 @@ import ContentsCardView from "@/view/ContentsCardView";
 import { fetchMainContents } from "@/server/data";
 
 const MainContentsView = async (props: { searchType?: string; searchDateType?: string }) => {
-  const contents = await fetchMainContents();
+  const contents = await fetchMainContents(props.searchType, props.searchDateType);
 
   return (
     <div className="mt-[10px] flex h-[70vh] w-full flex-wrap justify-between overflow-y-auto pr-[1%] tablet:h-[72vh]">
       {contents.map((item) => (
-        <ContentsCardView isFull={false} />
+        <ContentsCardView key={`contents-${item}`} isFull={false} />
       ))}
     </div>
   );
