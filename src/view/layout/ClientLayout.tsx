d2cddbf85588a0529457634bae342510638a19a2
inajung <ina.jung@coint.co.kr>
2024-01-03 13:39:43 +0900

    feat: RecoilRoot 삭제

--- src/view/layout/ClientLayout.tsx
+++ src/view/layout/ClientLayout.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 import { PropsWithChildren } from "react";
-import { RecoilRoot } from "recoil";
 import { roboto } from "@/view/layout/fonts";
 import HeaderView from "@/view/layout/HeaderView";
 import FooterView from "@/view/layout/Footer";
 
 const ClientLayout = (props: PropsWithChildren) => {
   return (
-    <RecoilRoot>
-      <body
-        className={`${roboto.className} gmarket bg-ccfd1dd pt-7 text-c1f295a dark:bg-c000000 dark:text-cffffff`}
-      >
-        <div className="mx-auto my-0 h-[89vh] w-[96vw] overflow-hidden rounded-3xl bg-cffffff dark:bg-cFFFFFF4C">
-          <HeaderView />
-          <main className="h-screen w-full px-10">{props.children}</main>
-        </div>
-        <FooterView />
-      </body>
-    </RecoilRoot>
+    <body
+      className={`${roboto.className} gmarket bg-ccfd1dd pt-7 text-c1f295a dark:bg-c000000 dark:text-cffffff`}
+    >
+      <div className="mx-auto my-0 h-[89vh] w-[96vw] overflow-hidden rounded-3xl bg-cffffff dark:bg-cFFFFFF4C">
+        <HeaderView />
+        <main className="h-screen w-full px-10">{props.children}</main>
+      </div>
+      <FooterView />
+    </body>
   );
 };
 
