955cc5398d1f0b77915032d3ccd68f9d0ea7fb6e
inajung <ina.jung@coint.co.kr>
2024-01-03 15:25:22 +0900

    feat: zustand middleware 로 sessionStorage 자동 저장 추가

--- src/store/token.ts
+++ src/store/token.ts
@@ -1,5 +1,5 @@
-import { isNil } from "lodash";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 // TODO :: NEXT-AUTH 사용 하면 불필요
 interface TokenState {
@@ -7,17 +7,15 @@ interface TokenState {
   setToken: (value: string | null) => void;
 }
 
-export const tokenState = create<TokenState>((set) => ({
-  token: null,
-  setToken: (value) => {
-    if (typeof value === "string") {
-      sessionStorage.setItem("X-ACCESS-TOKEN-ADMIN", value);
-    }
-
-    if (isNil(value)) {
-      sessionStorage.removeItem("X-ACCESS-TOKEN-ADMIN");
-    }
-
-    return set({ token: value });
-  },
-}));
+export const tokenState = create<TokenState>()(
+  persist(
+    (set, get) => ({
+      token: null,
+      setToken: (value) => set({ token: value }),
+    }),
+    {
+      name: "X-ACCESS-TOKEN-ADMIN",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
