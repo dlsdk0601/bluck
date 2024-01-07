1b56a67f7df102eadec91f9b6fd4806b775bdf4c
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-07 20:57:34 +0900

    feat: global prisma 생성

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalPrisma = global as typeof globalThis & { prisma: PrismaClient };
+
+// dev 와 production 에서 다른 값을 전달하기 위해 let 으로 한다.
+// eslint-disable-next-line import/no-mutable-exports
+let prisma: PrismaClient;
+
+// https://vercel.com/guides/nextjs-prisma-postgres
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalPrisma.prisma) {
+    globalPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalPrisma.prisma;
+}
+
+export default prisma;
