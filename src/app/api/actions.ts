1002fa8421ad8d0d85f7870ceb07b77b8e57fe0f
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-07 20:58:38 +0900

    feat: sign in action 함수 생성

---
+++ src/app/api/actions.ts
@@ -0,0 +1,19 @@
+import { AuthError } from "next-auth";
+import { signIn } from "@/app/api/auth/auth";
+
+export async function signInAction(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn("credentials", formData);
+  } catch (e) {
+    if (e instanceof AuthError) {
+      switch (e.type) {
+        case "CredentialsSignin":
+          return "일치하는 회원이 존재하지 않습니다.";
+        default:
+          return "로그인이 실패하였습니다.\n 잠시 후 다시 시도 해주세요.";
+      }
+    }
+
+    throw e;
+  }
+}
