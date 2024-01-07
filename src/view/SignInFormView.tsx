e934f12d9c81d2af54dc6d21cf252111941837a2
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-08 03:19:27 +0900

    feat: input 에 name 추가

--- src/view/SignInFormView.tsx
+++ src/view/SignInFormView.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import Image from "next/image";
+import { useFormState, useFormStatus } from "react-dom";
+import { signInAction } from "@/server/actions";
+import { isNotNil } from "@/ex/utils";
 
 const SignInFormView = () => {
-  // const { pending } = useFormStatus();
-  // const [error, dispatch] = useFormState(signInAction, undefined);
+  const [error, dispatch] = useFormState(signInAction, undefined);
 
   return (
-    <form className="w-4/5 mobile:w-full">
+    <form action={dispatch} className="w-4/5 mobile:w-full">
       <div className="mt-5 flex items-center justify-center overflow-hidden rounded-xl bg-cedeff6">
         <label htmlFor="id" className="bold w-1/5 pl-2 text-[14px] mobile:pl-3 mobile:text-[10px]">
           아이디
         </label>
         <input
           id="id"
+          name="email"
           className="h-12 w-4/5 bg-cedeff6 text-sm font-light focus:bg-none focus:outline-none mobile:h-10 "
-          type="text"
+          type="email"
         />
       </div>
       <div className="mt-5 flex items-center justify-center overflow-hidden rounded-xl bg-cedeff6">
@@ -27,14 +30,14 @@ const SignInFormView = () => {
         </label>
         <input
           id="password"
+          name="password"
           className="h-12 w-4/5 bg-cedeff6 text-sm font-light focus:bg-none focus:outline-none mobile:h-10 mobile:text-[11px]"
           type="password"
         />
       </div>
-      {/* {isNotNil(error) && ( */}
-      {/* <p className="mt-2 text-[10px] text-cff4500 mobile:text-[8px]">{error}</p> */}
-      <p className="mt-2 text-[10px] text-cff4500 mobile:text-[8px]">error</p>
-      {/* )} */}
+      {isNotNil(error) && (
+        <p className="mt-2 text-[10px] text-cff4500 mobile:text-[8px]">{error}</p>
+      )}
       <div className="mt-2 flex cursor-pointer items-center justify-start tablet:w-3/5">
         <figure className="relative h-5 w-5 mobile:h-4 mobile:w-4">
           <Image fill src="/assets/img/check.png" alt="check" />
@@ -44,14 +47,22 @@ const SignInFormView = () => {
         </label>
         <input id="login-check" type="checkbox" className="hidden opacity-0" />
       </div>
-      <button
-        type="submit"
-        // aria-disabled={pending}
-        className="mt-10 h-14 w-full rounded-xl border-2 border-solid border-c1f295a bg-none font-medium mobile:mt-8 mobile:h-8 mobile:text-lg"
-      >
-        로그인
-      </button>
+      <SignInButtonView />
     </form>
+  );
+};
+
+const SignInButtonView = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      aria-disabled={pending}
+      className="mt-10 h-14 w-full rounded-xl border-2 border-solid border-c1f295a bg-none font-medium mobile:mt-8 mobile:h-8 mobile:text-lg"
+    >
+      로그인
+    </button>
   );
 };
 
