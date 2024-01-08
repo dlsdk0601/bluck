1a5ecc0d13fca4d415a784b786491c6eecdff946
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-09 01:35:32 +0900

    feat: 참고 url 추가

--- src/server/auth/middleware.ts
+++ src/server/auth/middleware.ts
@@ -4,5 +4,6 @@ import { authConfig } from "@/server/auth/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
