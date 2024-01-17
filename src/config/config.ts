f4c8fb0eeb6e8b1268bd957d255e0f2006bf3787
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-18 00:41:18 +0900

    feat: 환경 변수 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,13 @@
 class Config {
+  // BASIC
   readonly baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
   readonly isDev = process.env.NEXT_PUBLIC_IS_DEV === "true";
+
+  // AWS
+  readonly awsAccessKey = process.env.AWS_ACCESS_KEY ?? "";
+  readonly awsSecretKey = process.env.AWS_SECRET_KEY ?? "";
+  readonly awsBucketName = process.env.AWS_BUCKET_NAME ?? "";
+  readonly awsRegion = process.env.AWS_REGION ?? "";
 }
 
 export const config = new Config();
