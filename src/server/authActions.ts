8d97ffa319692c49af3aff0a99a280f10deb1717
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-19 00:02:00 +0900

    feat: signInAction 에 이메일, 비밀번호 validation 추가

--- src/server/authActions.ts
+++ src/server/authActions.ts
@@ -1,10 +1,26 @@
 "use server";
 
 import { AuthError } from "next-auth";
+import { vEmail, vPassword } from "@/ex/validate";
+import { isNotNil } from "@/ex/utils";
 import { signIn } from "./auth/auth";
 
 export async function signInAction(prevState: string | undefined, formData: FormData) {
   try {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // email, password validation
+    const validEmail = vEmail(email);
+    if (isNotNil(validEmail)) {
+      return validEmail;
+    }
+
+    const validPassword = vPassword(password);
+    if (isNotNil(validPassword)) {
+      return validPassword;
+    }
+
     await signIn("credentials", formData);
   } catch (e) {
     if (e instanceof AuthError) {
