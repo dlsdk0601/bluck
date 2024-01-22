27bff2a4b9c0d9ad5d663de111af8fa0224e4c2d
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-23 00:45:40 +0900

    feat: input 모듈화

---
+++ src/view/auth/InputFieldView.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+
+const InputFieldView = (props: {
+  value: string;
+  onChange: (value: string) => void;
+  id: string;
+  label: string;
+  type?: "text" | "password" | "tel" | "email";
+  placeholder?: string;
+}) => {
+  return (
+    <>
+      <label htmlFor={props.id} className="w-1/5 pl-2 text-sm mobile:pl-4 mobile:text-[10px]">
+        {props.label}
+      </label>
+      <input
+        id={props.id}
+        type={props.type ?? "text"}
+        className="h-12 w-4/5 rounded-r-xl bg-ccfd1dd text-sm font-light focus:outline-none dark:bg-c000000 mobile:h-10"
+        placeholder={props.placeholder ?? "데이터를 입력해주세요."}
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default memo(InputFieldView);
