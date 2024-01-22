0d149f6dd2c7e830d0820e2bbba2cc22f059fbe5
dlsdk0601 <dlsdk0601@gmail.com>
2024-01-23 00:45:27 +0900

    feat: error 처리 수정

--- src/app/api/auth/find-id/route.ts
+++ src/app/api/auth/find-id/route.ts
@@ -3,6 +3,7 @@ import { isNil } from "lodash";
 import prisma from "@/lib/prisma";
 import { isBlank } from "@/ex/utils";
 import { vPhone } from "@/ex/validate";
+import { badRequestException, errorMessage, notFoundException } from "@/lib/errorEx";
 
 export interface FindIdReq {
   name: string;
@@ -17,15 +18,15 @@ export async function POST(req: NextRequest) {
   const body: FindIdReq = await req.json();
 
   if (isBlank(body.name)) {
-    return new Error("이름 실패");
+    return badRequestException(errorMessage.nameRequired);
   }
 
   if (isBlank(body.phone)) {
-    return new Error("휴대폰 실패");
+    return badRequestException(errorMessage.phoneRequired);
   }
 
   if (vPhone(body.phone)) {
-    return NextResponse.error();
+    return badRequestException(errorMessage.phoneBadFormat);
   }
 
   const user = await prisma.user.findFirst({
@@ -36,7 +37,7 @@ export async function POST(req: NextRequest) {
   });
 
   if (isNil(user)) {
-    return NextResponse.error();
+    return notFoundException(errorMessage.userNotFound);
   }
 
   return NextResponse.json({ id: user.email });
