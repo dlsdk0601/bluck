960a79a1863b0567f443584e81f978719f46ea96
inajung <ina.jung@coint.co.kr>
2024-01-23 11:31:26 +0900

    feat: 아이디 찾기 action 생성

--- src/server/authActions.ts
+++ src/server/authActions.ts
@@ -1,9 +1,11 @@
 "use server";
 
 import { AuthError } from "next-auth";
-import { vEmail, vPassword } from "@/ex/validate";
-import { isNotNil } from "@/ex/utils";
+import { isNil, isString } from "lodash";
+import { vEmail, vPassword, vPhone } from "@/ex/validate";
+import { isBlank, isNotNil } from "@/ex/utils";
 import { errorMessage } from "@/lib/errorEx";
+import prisma from "@/lib/prisma";
 import { signIn } from "./auth/auth";
 
 export async function signInAction(prevState: string | undefined, formData: FormData) {
@@ -27,10 +29,61 @@ export async function signInAction(prevState: string | undefined, formData: Form
     if (e instanceof AuthError) {
       switch (e.type) {
         case "CredentialsSignin":
-          return errorMessage.userNotFound;
+          return errorMessage.USER_NOT_FOUND;
         default:
-          return errorMessage.signInFailed;
+          return errorMessage.SIGN_ING_FAILED;
       }
+    }
+
+    throw e;
+  }
+}
+
+interface FindIdActionRes {
+  error: string | null;
+  data: null | { id: string };
+}
+
+export async function findIdAction(
+  prevState: FindIdActionRes | undefined,
+  formData: FormData,
+): Promise<FindIdActionRes> {
+  try {
+    const name = formData.get("name");
+    const phone = formData.get("phone");
+
+    if (isNil(name) || isBlank(name)) {
+      return { error: errorMessage.NAME_REQUIRED, data: null };
+    }
+
+    if (isNil(phone) || isBlank(phone)) {
+      return { error: errorMessage.PHONE_REQUIRED, data: null };
+    }
+
+    if (!isString(name)) {
+      return { error: errorMessage.ONLY_STRING("이름"), data: null };
+    }
+
+    if (!isString(phone)) {
+      return { error: errorMessage.ONLY_STRING("휴대폰"), data: null };
+    }
+
+    if (vPhone(phone)) {
+      return { error: errorMessage.PHONE_BAD_FORMAT, data: null };
+    }
+
+    const user = await prisma.user.findFirst({
+      where: { name, phone },
+    });
+
+    if (isNil(user)) {
+      return { error: errorMessage.USER_NOT_FOUND, data: null };
+    }
+
+    return { error: null, data: { id: user.email } };
+  } catch (e) {
+    if (e instanceof Error) {
+      return { error: e.message, data: null };
     }
 
     throw e;
