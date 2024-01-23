6592b37f77a79d0f57b5b36479b6602609475efb
inajung <ina.jung@coint.co.kr>
2024-01-23 13:33:02 +0900

    feat: 비밀번호 찾기 form view 컴포넌트 생성

---
+++ src/view/auth/FindPasswordFormView.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { findPasswordAction } from "@/server/authActions";
+import { isNotBlank, isNotNil } from "@/ex/utils";
+
+const FindPasswordFormView = () => {
+  const [res, dispatch] = useFormState(findPasswordAction, undefined);
+
+  if (isNotNil(res?.data?.result) && res?.data?.result) {
+    return <p>이메일로 비밀번호 전송하였습니다.</p>;
+  }
+
+  return (
+    <form action={dispatch} className="w-4/5 mobile:w-full">
+      <div className="mt-3 flex items-center justify-center rounded-xl bg-ccfd1dd dark:bg-c000000">
+        <label htmlFor="name" className="w-1/5 pl-2 text-sm mobile:pl-4 mobile:text-[10px]">
+          이름
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          className="h-12 w-4/5 rounded-r-xl bg-ccfd1dd text-sm font-light focus:outline-none dark:bg-c000000 mobile:h-10"
+          placeholder="이름을 입력해주세요."
+        />
+      </div>
+      <div className="mt-3 flex items-center justify-center rounded-xl bg-ccfd1dd dark:bg-c000000">
+        <label htmlFor="email" className="w-1/5 pl-2 text-sm mobile:pl-4 mobile:text-[10px]">
+          아이디
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          className="h-12 w-4/5 rounded-r-xl bg-ccfd1dd text-sm font-light focus:outline-none dark:bg-c000000 mobile:h-10"
+          placeholder="아이디를 입력해주세요."
+        />
+      </div>
+      {isNotBlank(res?.error) && (
+        <p className="mt-2 text-xs text-cff4500 mobile:mt-1 mobile:text-[9px]">{res?.error}</p>
+      )}
+      <button
+        type="submit"
+        className="mt-12 h-14 w-full cursor-pointer rounded-2xl border-2 border-solid border-c1f295a dark:border-cffffff mobile:mt-8 mobile:h-10 mobile:text-[12px]"
+      >
+        비밀번호 찾기
+      </button>
+    </form>
+  );
+};
+
+export default FindPasswordFormView;
