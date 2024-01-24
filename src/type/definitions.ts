ce023b23cf1916a3bcd11cf58c917f136c97eaf9
inajung <ina.jung@coint.co.kr>
2024-01-24 15:00:23 +0900

    feat: action 함수에 대한 타입 정의 추상화

--- src/type/definitions.ts
+++ src/type/definitions.ts
@@ -1,8 +1,22 @@
-export type User = {
-  email: string;
-  password: string;
-  name: string;
-  birthday: Date;
-  phone: string;
-  message: string;
-};
+interface R<T> {
+  error: string | null;
+  data: T;
+}
+
+export type Res<T> = R<T | null>;
+
+export function ok<T>(res: T): Res<T> {
+  return { error: null, data: res };
+}
+
+export function err(error: string): Res<null> {
+  return { error, data: null };
+}
+
+type BaseActionFunction<T> = (prevState: Res<T> | null, formData: FormData) => Promise<Res<T>>;
+
+export type SignInActionType = BaseActionFunction<{ result: boolean }>;
+
+export type FindIdActionType = BaseActionFunction<{ id: string }>;
+
+export type FindPasswordType = BaseActionFunction<{ result: string }>;
