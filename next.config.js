f79764155153300b2f4fd829835d3c99d1796613
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-01 04:27:17 +0900

    feat: aws host 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nest-ina-boilerplate.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
