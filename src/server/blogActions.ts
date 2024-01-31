18d76c1bdfaff5d29909f20b5e644af3ef959ae3
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-01 04:28:26 +0900

    feat: getBlogsAction 타입 정의 후, API 생성

--- src/server/blogActions.ts
+++ src/server/blogActions.ts
@@ -1,11 +1,50 @@
 "use server";
 
-import { range } from "lodash";
+import { Prisma } from "@prisma/client";
+import { GetBlogsActionResItem, getBlogsActionType, ok } from "@/type/definitions";
+import { awsModel } from "@/lib/aws";
+import { d1 } from "@/ex/dateEx";
+import prisma from "@/lib/prisma";
+import { PAGE_LIMIT, Pagination } from "@/ex/paginationEx";
 
-export async function fetchMainContents(searchType?: string, searchDateType?: string) {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 3000);
+export const getBlogsAction: getBlogsActionType = async (page, searchType, searchDateType) => {
+  // TODO :: 검색 조건
+  const query: Prisma.blogFindManyArgs = {
+    where: {},
+  };
+
+  const [blogs, count] = await prisma.$transaction([
+    prisma.blog.findMany({
+      select: {
+        pk: true,
+        title: true,
+        created_at: true,
+        user: {
+          select: {
+            name: true,
+            main_image: true,
+          },
+        },
+      },
+      skip: (page - 1) * PAGE_LIMIT,
+      take: PAGE_LIMIT,
+      where: query.where,
+    }),
+    prisma.blog.count({ where: query.where }),
+  ]);
+
+  const pagination = new Pagination<GetBlogsActionResItem>(count, page);
+  pagination.rows = blogs.map((blog) => ({
+    pk: blog.pk,
+    title: blog.title,
+    createAt: d1(blog.created_at),
+    user: {
+      profile: awsModel.toFileSet(blog.user.main_image),
+      name: blog.user.name,
+    },
+  }));
+
+  return ok({
+    blogs: pagination,
   });
-
-  return range(0, 15);
-}
+};
--- src/type/definitions.ts
+++ src/type/definitions.ts
@@ -1,3 +1,6 @@
+import { Fileset } from "@/lib/aws";
+import { PaginationType } from "@/ex/paginationEx";
+
 interface R<T> {
   error: string | null;
   data: T;
@@ -13,12 +16,32 @@ export function err(error: string): Res<null> {
   return { error, data: null };
 }
 
-type BaseActionFunction<T> = (prevState: Res<T> | null, formData: FormData) => Promise<Res<T>>;
+type BaseFormActionFunction<T> = (prevState: Res<T> | null, formData: FormData) => Promise<Res<T>>;
 
-export type SignInActionType = BaseActionFunction<{ result: boolean }>;
+export type SignInActionType = BaseFormActionFunction<{ result: boolean }>;
 
-export type FindIdActionType = BaseActionFunction<{ id: string }>;
+export type FindIdActionType = BaseFormActionFunction<{ id: string }>;
 
-export type FindPasswordActionType = BaseActionFunction<{ result: string }>;
+export type FindPasswordActionType = BaseFormActionFunction<{ result: string }>;
 
-export type SignUpActionType = BaseActionFunction<{ result: boolean }>;
+export type SignUpActionType = BaseFormActionFunction<{ result: boolean }>;
+
+export interface GetBlogsActionResItem {
+  pk: number;
+  title: string;
+  createAt: string;
+  user: {
+    profile: Fileset;
+    name: string;
+  };
+}
+
+export interface GetBlogsActionRes {
+  blogs: PaginationType<GetBlogsActionResItem>;
+}
+
+export type getBlogsActionType = (
+  page: number,
+  searchType: string | undefined,
+  searchDateType: string | undefined,
+) => Promise<Res<GetBlogsActionRes>>;
