4893d1f6336700aaccdf47be54639f9dad5142f4
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-07 23:36:18 +0900

    feat: 개발 단계에서 사용할 로그인 버튼 추가

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { Metadata } from "next";
 import { Urls } from "@/url/url.g";
 import SignInFormView from "@/view/auth/SignInFormView";
+import { signIn } from "@/server/auth/auth";
+import { config } from "@/config/config";
 
 export const metadata: Metadata = {
   title: "sign-in",
@@ -10,6 +12,7 @@ export const metadata: Metadata = {
 const SignInPage = () => {
   return (
     <section className="mx-auto my-0 flex h-4/5 w-2/5 flex-col items-center justify-center tablet:w-[75%] mobile:w-full">
+      {config.isDev && <TestSignInButtonView />}
       <SignInFormView />
       <div className="mx-auto mt-8 flex w-4/5 items-center justify-around mobile:mt-5">
         <Link
@@ -34,6 +37,24 @@ const SignInPage = () => {
         </Link>
       </div>
     </section>
+  );
+};
+
+const TestSignInButtonView = () => {
+  return (
+    <form
+      action={async () => {
+        "use server";
+
+        const formData = new FormData();
+        formData.set("email", "test@test.com");
+        formData.set("password", "1234");
+
+        await signIn("credentials", formData);
+      }}
+    >
+      <button type="submit">test</button>
+    </form>
   );
 };
 
