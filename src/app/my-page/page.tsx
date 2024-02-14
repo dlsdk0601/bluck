c57e393a4aef0378542b69a8ea6d8149b85b1066
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-15 02:11:47 +0900

    feat: my-page 퍼블 완성 및 action 연동

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,30 +1,37 @@
-import Image from "next/image";
 import Link from "next/link";
+import { isNil } from "lodash";
+import BlogCardView from "@/view/blog/BlogCardView";
+import { myPageInitAction } from "@/server/myPageAction";
+import { isNotNil } from "@/ex/utils";
+import { Urls } from "@/url/url.g";
+import MyPageProfileView from "@/view/myPage/MyPageProfileView";
+import MyPageTagsView from "@/view/myPage/MyPageTagsView";
 
-const Page = () => {
-  return (
-    <>
-      {/* 프로필 */}
-      <div className="flex items-start justify-start bg-cedeff6 px-7 py-5">
-        <figure className="relative h-24 w-24">
-          <Image src="/assets/img/dog.png" layout="fill" alt="porfile" />
-        </figure>
-        <div className="ml-4 flex w-4/5 flex-col items-start justify-between">
-          <div>
-            <span className="text-xl">이름</span> | <span className="text-sm">별명</span>
-          </div>
-          <div>
-            <h3 className="mt-1 text-sm">상태 메세지</h3>
-            <p className="mb-2 mt-1 text-xs opacity-70">
-              자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개
-              자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개
-            </p>
-          </div>
-          <Link href="/" className="text- text-sm mobile:text-xs">
-            회원 정보 변경
+const Page = async (props: { searchParams?: { tags?: number[] } }) => {
+  console.log("props.searchParams?.tags");
+  console.log(props.searchParams?.tags);
+  const res = await myPageInitAction();
+
+  if (isNotNil(res.error) || isNil(res.data)) {
+    return (
+      <div className="mx-auto flex h-[75vh] w-3/5 items-center justify-center mobile:w-[95%]">
+        <div>
+          <p className="text-center">{res.error}</p>
+          <Link
+            href={Urls.page.url()}
+            className="mt-3 block rounded border-[1px] border-solid border-c1f295a py-2 text-center dark:border-cffffff"
+          >
+            Home 으로 가기
           </Link>
         </div>
       </div>
+    );
+  }
+
+  return (
+    <>
+      {/* 프로필 */}
+      <MyPageProfileView user={res.data.user} />
 
       {/*  하단 */}
       <div className="mt-5 flex h-2/3 items-start justify-between">
@@ -33,9 +40,17 @@ const Page = () => {
           <p className="mb-2 h-11 w-full rounded-xl bg-c1f295a py-2 text-center text-lg text-cffffff opacity-90">
             태그
           </p>
-          <ul className="h-[40vh] w-full rounded-xl bg-ccfd1dd px-3 py-6">
-            <li className="text-cffffff">태그</li>
-          </ul>
+          <MyPageTagsView tags={res.data.tags} />
+        </div>
+        <div className="w-4/5 pt-2">
+          <p className="mb-5 border-b-[1px] border-solid border-c1f295a pb-1">
+            내가 쓴 글 총 {res.data.blogs.length}개
+          </p>
+          <div className="flex h-[47vh] flex-wrap justify-between overflow-y-scroll">
+            {res.data.blogs.map((blog) => (
+              <BlogCardView key={`my-page-blog-${blog.pk}`} blog={blog} isFull />
+            ))}
+          </div>
         </div>
       </div>
     </>
