006c74532b1b02e3b491d1dc8cdbab78a8ddda68
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-17 00:03:04 +0900

    feat: 태그 클릭시 쿼리 array 처리 추가

--- src/view/myPage/MyPageTagsView.tsx
+++ src/view/myPage/MyPageTagsView.tsx
@@ -1,29 +1,54 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
 import classNames from "classnames";
+import { pull } from "lodash";
 import { MyPageTag } from "@/type/definitions";
 import { Urls } from "@/url/url.g";
 
 const MyPageTagsView = (props: { tags: MyPageTag[] }) => {
+  const router = useRouter();
   const query = useSearchParams();
+  const tags = query.getAll("tags");
 
   return (
-    <ul className="h-[47vh] w-full rounded-xl bg-ccfd1dd px-3 py-6">
-      {props.tags.map((tag) => (
-        <li
-          key={`my-page-tag-${tag.pk}`}
-          className={classNames("m-1 rounded bg-c1f295a px-2 py-1 text-center text-cffffff", {
-            "opacity-50": !query.getAll("tags").includes(tag.pk.toString()),
-          })}
-        >
-          <Link href={Urls["my-page"].page.setQuery(query, { tags: tag.pk.toString() })}>
-            {tag.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div className="h-full w-1/6">
+      <p className="mb-2 h-11 w-full rounded-xl bg-c1f295a py-2 text-center text-lg text-cffffff opacity-90">
+        태그
+      </p>
+      <ul className="h-[47vh] w-full rounded-xl bg-ccfd1dd px-3 py-6">
+        {props.tags.map((tag) => (
+          <li
+            key={`my-page-tag-${tag.pk}`}
+            className={classNames("m-1 rounded bg-c1f295a px-2 py-1 text-center text-cffffff", {
+              "opacity-50": !tags.includes(tag.pk.toString()),
+            })}
+          >
+            <button
+              type="button"
+              onClick={() => {
+                if (tags.includes(tag.pk.toString())) {
+                  pull(tags, tag.pk.toString());
+                } else {
+                  tags.push(tag.pk.toString());
+                }
+
+                let queryString = "";
+
+                // array 형으로 query 를 set 해주는 method 가 따로 없어서 string 으로 바로 처리 한다.
+                for (let i = 0; i < tags.length; i++) {
+                  queryString += `tags=${tags[i]}${i === tags.length - 1 ? "" : "&"}`;
+                }
+
+                router.replace(`${Urls["my-page"].page.url()}?${queryString}`);
+              }}
+            >
+              {tag.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
