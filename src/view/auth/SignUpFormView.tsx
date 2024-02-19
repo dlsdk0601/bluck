a8f45209407eff6a54bc4916a742d3ca2c448100
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-20 02:02:07 +0900

    feat: props 데이터로 분기 처리 추가

--- src/view/auth/SignUpFormView.tsx
+++ src/view/auth/SignUpFormView.tsx
@@ -8,6 +8,7 @@ import classNames from "classnames";
 import { CheckCircleIcon as OutlineCheckCircleIcon } from "@heroicons/react/24/outline";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { signUpAction } from "@/server/authActions";
 import { vFileExtension } from "@/ex/validate";
 import { isNotNil } from "@/ex/utils";
@@ -15,20 +16,28 @@ import { api } from "@/lib/axios";
 import { Fileset } from "@/lib/aws";
 import { Urls } from "@/url/url.g";
 import { isLockState } from "@/store/isLock";
+import { ShowUserActionRes } from "@/type/definitions";
 
-const SignUpFormView = () => {
+const SignUpFormView = (props: { data?: ShowUserActionRes }) => {
+  const router = useRouter();
   const [res, dispatch] = useFormState(signUpAction, null);
 
   if (isNotNil(res?.data?.result) && res?.data?.result) {
     return (
       <div className="mx-auto flex h-4/5 w-2/5 flex-col items-center justify-center tablet:w-3/4 mobile:w-full">
-        <p>회원가입이 정상적으로 처리되었습니다.</p>
-        <Link
-          className="mt-5 cursor-pointer text-xs font-medium tablet:text-[10px] mobile:text-[9px]"
-          href={Urls["sign-in"].page.url()}
-        >
-          로그인
-        </Link>
+        <p>
+          {isNil(props.data)
+            ? "회원가입이 정상적으로 처리되었습니다."
+            : "정보가 정상적으로 수정되었습니다."}
+        </p>
+        {isNil(props.data) && (
+          <Link
+            className="mt-5 cursor-pointer text-xs font-medium tablet:text-[10px] mobile:text-[9px]"
+            href={Urls["sign-in"].page.url()}
+          >
+            로그인
+          </Link>
+        )}
       </div>
     );
   }
@@ -36,9 +45,14 @@ const SignUpFormView = () => {
   return (
     <form
       action={dispatch}
-      className="mx-auto flex h-4/6 max-w-4xl items-start justify-center overflow-y-auto overflow-x-hidden mobile:block"
+      className={classNames(
+        "mx-auto flex h-4/6 max-w-4xl items-start justify-center overflow-y-auto overflow-x-hidden mobile:block",
+        {
+          "mt-16": isNotNil(props.data),
+        },
+      )}
     >
-      <FileUploadView />
+      <FileUploadView profile={props.data?.profile} />
       <div className="mx-5 flex w-8/12 flex-wrap items-center justify-between mobile:mx-auto mobile:w-5/6">
         <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
           <label
@@ -51,40 +65,45 @@ const SignUpFormView = () => {
             id="email"
             type="email"
             name="email"
+            defaultValue={props.data?.email}
             className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
             placeholder="이메일을 입력해주세요."
           />
         </div>
-        <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
-          <label
-            htmlFor="password"
-            className="flex h-10 w-1/5 items-center bg-ccfd1dd pl-2 text-sm font-medium leading-3 dark:bg-c000000 mobile:pl-4 mobile:text-[10px]"
-          >
-            비밀번호
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
-            placeholder="비밀번호를 입력해주세요."
-          />
-        </div>
-        <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
-          <label
-            htmlFor="confirm-password"
-            className="flex h-10 w-1/5 items-center bg-ccfd1dd pl-2 text-sm font-medium leading-3 dark:bg-c000000 mobile:pl-4 mobile:text-[10px]"
-          >
-            재확인
-          </label>
-          <input
-            id="confirm-password"
-            name="confirm-password"
-            type="password"
-            className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
-            placeholder="비밀번호를 다시 입력해주세요."
-          />
-        </div>
+        {isNil(props.data) && (
+          <>
+            <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
+              <label
+                htmlFor="password"
+                className="flex h-10 w-1/5 items-center bg-ccfd1dd pl-2 text-sm font-medium leading-3 dark:bg-c000000 mobile:pl-4 mobile:text-[10px]"
+              >
+                비밀번호
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
+                placeholder="비밀번호를 입력해주세요."
+              />
+            </div>
+            <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
+              <label
+                htmlFor="confirm-password"
+                className="flex h-10 w-1/5 items-center bg-ccfd1dd pl-2 text-sm font-medium leading-3 dark:bg-c000000 mobile:pl-4 mobile:text-[10px]"
+              >
+                재확인
+              </label>
+              <input
+                id="confirm-password"
+                name="confirm-password"
+                type="password"
+                className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
+                placeholder="비밀번호를 다시 입력해주세요."
+              />
+            </div>{" "}
+          </>
+        )}
         <div className="mb-3 flex w-full items-center justify-start overflow-hidden rounded-xl mobile:mb-1">
           <label
             htmlFor="name"
@@ -96,6 +115,7 @@ const SignUpFormView = () => {
             id="name"
             name="name"
             type="text"
+            defaultValue={props.data?.name}
             className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
             placeholder="이름을 입력해주세요."
           />
@@ -111,6 +131,7 @@ const SignUpFormView = () => {
             id="text"
             name="birthday"
             type="text"
+            defaultValue={props.data?.birthday}
             className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
             placeholder="생년월일 6자리 입력해주세요."
           />
@@ -126,6 +147,7 @@ const SignUpFormView = () => {
             id="tel"
             name="phone"
             type="tel"
+            defaultValue={props.data?.phone}
             className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
             placeholder="- 없이 입력해주세요."
           />
@@ -141,6 +163,7 @@ const SignUpFormView = () => {
             id="message"
             name="message"
             type="text"
+            defaultValue={props.data?.message}
             className="h-10 w-4/5 rounded-r-xl border-none bg-ccfd1dd focus:outline-none dark:bg-c000000"
             placeholder="상태메시지를 입력해주세요."
           />
@@ -155,11 +178,12 @@ const SignUpFormView = () => {
           <textarea
             id="introduce"
             name="introduce"
+            defaultValue={props.data?.introduce}
             className="h-5/6 w-4/5 rounded-xl p-2 focus:outline-none"
             placeholder="자기소개를 입력해주세요."
           />
         </div>
-        <SignUpPersonalInformationRadioBoxView />
+        {isNil(props.data) && <SignUpPersonalInformationRadioBoxView />}
 
         {isNotNil(res?.error) && (
           <span className="mb-3 pl-2 text-[10px] text-cff4500 mobile:mb-2">{res?.error}</span>
@@ -168,10 +192,11 @@ const SignUpFormView = () => {
           <button
             type="button"
             className="h-10 w-72 rounded-2xl border-2 border-solid border-c1f295a dark:border-cffffff mobile:h-8 mobile:text-[10px]"
+            onClick={() => router.back()}
           >
             취소
           </button>
-          <SignUpButtonView />
+          <SignUpButtonView isSign={isNotNil(props.data)} />
         </div>
       </div>
     </form>
@@ -199,7 +224,7 @@ const SignUpPersonalInformationRadioBoxView = () => {
   );
 };
 
-const SignUpButtonView = () => {
+const SignUpButtonView = (props: { isSign: boolean }) => {
   const { pending } = useFormStatus();
   const setIsLock = isLockState((state) => state.setIsLock);
 
@@ -213,13 +238,13 @@ const SignUpButtonView = () => {
       aria-disabled={pending}
       className="h-10 w-72 rounded-2xl border-2 border-solid border-c1f295a dark:border-cffffff mobile:h-8 mobile:text-[10px]"
     >
-      회원가입
+      {props.isSign ? "수정" : "회원가입"}
     </button>
   );
 };
 
-const FileUploadView = () => {
-  const [fileSet, setFileSet] = useState<Fileset | null>(null);
+const FileUploadView = (props: { profile?: Fileset }) => {
+  const [fileSet, setFileSet] = useState<Fileset | null>(props.profile ?? null);
 
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -273,7 +298,7 @@ const FileUploadView = () => {
           className="hidden"
         />
       </label>
-      <input type="text" value={fileSet?.uuid} name="uuid" className="hidden opacity-0" readOnly />
+      <input type="hidden" value={fileSet?.uuid} name="uuid" readOnly />
     </>
   );
 };
