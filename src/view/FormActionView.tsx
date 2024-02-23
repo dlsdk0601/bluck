67e71e496afe6523c586262b2144d62ea00bab42
dlsdk0601 <dlsdk0601@gmail.com>
2024-02-24 00:53:29 +0900

    feat: 중복되는 FormAction 을 위해 FormActionView 생성

---
+++ src/view/FormActionView.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ComponentType, Fragment, useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+import { BaseFormActionFunction, FormActionViewProps } from "@/type/definitions";
+
+const FormActionView = <T,>(
+  action: BaseFormActionFunction<T>,
+  Component: ComponentType<FormActionViewProps<T>>,
+): (() => JSX.Element) => {
+  return function FormAction() {
+    const [res, dispatch] = useFormState(action, null);
+    const [child, setChild] = useState(() => <></>);
+
+    useEffect(() => {
+      setChild(<Component res={res} dispatch={dispatch} />);
+    }, [res]);
+
+    return <>{child}</>;
+  };
+};
+
+export default FormActionView;
