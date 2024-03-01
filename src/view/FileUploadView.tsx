58c2e24d80165f4b67ff6de7428d9442a463a8d3
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-01 23:59:52 +0900

    feat: props 변경 및 확장성 있게 onChage 추가

--- src/view/FileUploadView.tsx
+++ src/view/FileUploadView.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { isNil, isString } from "lodash";
 import { blobToBase64String } from "blob-util";
 import classNames from "classnames";
@@ -7,16 +7,10 @@ import { vFileExtension } from "@/ex/validate";
 import { isNotNil } from "@/ex/utils";
 import { api } from "@/lib/axios";
 
-const FileUploadView = (props: { profile?: Fileset }) => {
-  // uuid input 을 ref 로 처리한 이유는 onChange 없이 value 를 바꾸면 react 가 error 를 뱉는다.
-  const ref = useRef<HTMLInputElement>(null);
+const FileUploadView = (props: { profile?: Fileset; onChange: (fileSet: Fileset) => void }) => {
   const [fileSet, setFileSet] = useState<Fileset | null>(props.profile ?? null);
 
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (isNil(ref.current)) {
-      return;
-    }
-
     const { files } = e.target;
 
     if (isNil(files)) {
@@ -43,34 +37,31 @@ const FileUploadView = (props: { profile?: Fileset }) => {
     }
 
     setFileSet(res.fileSet);
-    ref.current.value = res.fileSet.uuid;
+    props.onChange(res.fileSet);
   };
 
   return (
-    <>
-      <label
-        htmlFor="profile"
-        style={isNotNil(fileSet?.url) ? { backgroundImage: `url(${fileSet.url})` } : undefined}
-        className={classNames(
-          "flex h-48 w-48 cursor-pointer items-center justify-center rounded-lg border-none mobile:mx-auto mobile:my-3 mobile:h-full mobile:w-full",
-          {
-            "bg-ccfd1dd dark:bg-c000000": isNil(fileSet),
-            "bg-cover bg-center bg-no-repeat": isNotNil(fileSet),
-          },
-        )}
-      >
-        {isNil(fileSet) ? "\u002B" : ""}
-        <input
-          type="file"
-          onChange={(e) => onChange(e)}
-          accept="image/*"
-          id="profile"
-          name="profile"
-          className="hidden"
-        />
-      </label>
-      <input ref={ref} defaultValue={props.profile?.uuid} type="hidden" name="uuid" readOnly />
-    </>
+    <label
+      htmlFor="profile"
+      style={isNotNil(fileSet?.url) ? { backgroundImage: `url(${fileSet.url})` } : undefined}
+      className={classNames(
+        "flex h-48 w-48 cursor-pointer items-center justify-center rounded-lg border-none mobile:mx-auto mobile:my-3 mobile:h-full mobile:w-full",
+        {
+          "bg-ccfd1dd dark:bg-c000000": isNil(fileSet),
+          "bg-cover bg-center bg-no-repeat": isNotNil(fileSet),
+        },
+      )}
+    >
+      {isNil(fileSet) ? "\u002B" : ""}
+      <input
+        type="file"
+        onChange={(e) => onChange(e)}
+        accept="image/*"
+        id="profile"
+        name="profile"
+        className="hidden"
+      />
+    </label>
   );
 };
 
