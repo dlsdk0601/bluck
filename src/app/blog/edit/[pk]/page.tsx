80c3f1dbfa1e5259247ef819de86c49a54e11617
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-03 23:10:33 +0900

    style: style 반영

--- src/app/blog/edit/[pk]/page.tsx
+++ src/app/blog/edit/[pk]/page.tsx
@@ -2,7 +2,7 @@ import BlogEditorView from "@/view/blog/BlogEditorView";
 
 const BlogEditPage = () => {
   return (
-    <div>
+    <div className="mx-auto my-0 flex h-4/5 w-2/5 flex-col items-center justify-center tablet:w-[75%] mobile:w-full">
       <BlogEditorView />
     </div>
   );
