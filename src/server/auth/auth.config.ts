f1149ed16099032f0f3ad7db2370db2025dc1419
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-06 23:07:16 +0900

    refactor: Urls 모두 제거 후 하드 코딩으로 수정 (build 버그)

--- src/server/auth/auth.config.ts
+++ src/server/auth/auth.config.ts
@@ -1,44 +1,35 @@
 import { NextAuthConfig } from "next-auth";
 import { NextResponse } from "next/server";
-import { isNotNil } from "@/ex/utils";
-import { Urls } from "@/url/url.g";
+import { config } from "@/config/config";
 
 // middleware 는 next 가 알아서 찾아서 적용 한다.
 // cna 를 할때 src 폴더를 할꺼냐 말꺼냐로 middleware 파일 위치가 다르다
 // src => src 폴더 안
 // src (x) => root 폴더
+// build 시 edge function 에서 lodash 지원을 하지 않는다. Urls 는 lodash 천국이기에 하드 코딩으로 간다
 
 export const authConfig = {
   pages: {
-    signIn: Urls["sign-in"].page.url(),
+    signIn: "/sign-in",
   },
   callbacks: {
     // false 를 return 하면 sign-in 으로 리다이렉트 된다.
     authorized({ auth, request: { nextUrl } }) {
       const user = auth?.user;
-      const isSign = isNotNil(user);
+      const isSign = !!user;
 
-      const signOutExceptions = [
-        Urls["my-page"].show.page.url(),
-        Urls["my-page"].user.edit.page.url(),
-        Urls["my-page"].password.edit.page.url(),
-      ];
+      const signOutExceptions = ["/my-page/show", "/my-page/user/edit", "my-page/password/edit"];
 
       // 비로그인 유저 접근 제한
       if (signOutExceptions.includes(nextUrl.pathname) && !isSign) {
-        return isNotNil(user);
+        return isSign;
       }
 
-      const signInExceptions = [
-        Urls["sign-in"].page.url(),
-        Urls["sign-up"].page.url(),
-        Urls["find-id"].page.url(),
-        Urls["find-password"].page.url(),
-      ];
+      const signInExceptions = ["/sign-in", "/sign-up", "/find-id", "/find-password"];
 
       // 로그인 유저 접근 제한
       if (signInExceptions.includes(nextUrl.pathname) && isSign) {
-        return NextResponse.redirect(Urls.page.fullUrl);
+        return NextResponse.redirect(`${config.baseUrl}/`);
       }
 
       return true;
