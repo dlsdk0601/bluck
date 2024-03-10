60c113d6d6021ec1d0f518abeae7428de30f5914
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-11 00:23:12 +0900

    feat: textarea 에 review state 달고, 로딩 처리 추가

--- src/view/blog/BlogReviewView.tsx
+++ src/view/blog/BlogReviewView.tsx
@@ -2,21 +2,51 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { isNil, isString } from "lodash";
 import { ReviewBlog } from "@/type/definitions";
 import { d1 } from "@/ex/dateEx";
+import { api } from "@/lib/axios";
+import { preventDefaulted } from "@/ex/utils";
+import { isLockState } from "@/store/isLock";
+import { userState } from "@/store/user";
+import { ERR } from "@/lib/errorEx";
 
-const BlogReviewView = (props: { reviews: ReviewBlog[] }) => {
+const BlogReviewView = (props: { blogPk: number; reviews: ReviewBlog[] }) => {
+  const setIsLock = isLockState((state) => state.setIsLock);
+  const user = userState((state) => state.user);
+  const [review, setReview] = useState("");
   const [reviews, setReviews] = useState<ReviewBlog[]>([]);
 
   useEffect(() => {
     setReviews([...props.reviews]);
   }, [props]);
 
+  const onSubmit = async () => {
+    if (isNil(user)) {
+      return alert(ERR.NOT_SIGN_USER);
+    }
+
+    setIsLock(true);
+    const res = await api.newBlogReview({ pk: props.blogPk, review });
+
+    if (isString(res)) {
+      return alert(res);
+    }
+
+    setReviews([...res.reviews]);
+    setReview("");
+    setIsLock(false);
+  };
+
   return (
     <div className="mx-auto w-11/12">
-      <form>
+      <form onSubmit={preventDefaulted(() => onSubmit())}>
         <p>{reviews.length}개의 댓글</p>
-        <textarea className="mt-2 h-[80px] w-full resize-none rounded border border-c4c547b p-1 focus:outline-none" />
+        <textarea
+          className="mt-2 h-[80px] w-full resize-none rounded border border-c4c547b p-1 focus:outline-none"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        />
         <div className="mt-2 flex justify-end">
           <button type="submit" className="rounded-xl bg-c1f295a px-2 py-1 text-xs text-cffffff">
             댓글 작성
@@ -42,7 +72,7 @@ const BlogReviewView = (props: { reviews: ReviewBlog[] }) => {
                 <p className="mt-2 text-xs">{d1(review.createAt)}</p>
               </div>
             </div>
-            <p className="mt-5">{review.review}</p>
+            <p className="mt-5 whitespace-pre-wrap">{review.review}</p>
           </li>
         ))}
       </ul>
