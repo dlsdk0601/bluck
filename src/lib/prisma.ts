f1268d6dd10e3bbc0aaeaf96816fbcbd91bc1b2e
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-13 22:49:36 +0900

    feat: 개발 단계에서 log 에 query 가 보이게 설정 추가

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!globalPrisma.prisma) {
-    globalPrisma.prisma = new PrismaClient();
+    globalPrisma.prisma = new PrismaClient({ log: ["query"] });
   }
   prisma = globalPrisma.prisma;
 }
