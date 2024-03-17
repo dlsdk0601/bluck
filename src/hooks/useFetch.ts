7ae6f4717c2cbc622d6e4ab72ded5ae23a4a4809
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-17 23:59:55 +0900

    feat: export 추가

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,7 +2,7 @@ import { isLockState } from "@/store/isLock";
 
 type ApiParameter<T, U> = (req: T) => Promise<U | string>;
 
-const useFetch = <T, U>(api: ApiParameter<T, U>) => {
+export const useFetch = <T, U>(api: ApiParameter<T, U>) => {
   const setIsLock = isLockState((s) => s.setIsLock);
 
   const onFetch = async (req: T) => {
