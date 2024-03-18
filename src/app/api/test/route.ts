7447b8416ac249f1d67c05254855d7a75b639d85
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-18 21:58:07 +0900

    feat: test API 에서 console.log 제거

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { getHash } from "@/ex/bcryptEx";
 
 export async function POST(req: NextRequest) {
-  console.log(req.body);
   const password = await getHash("1234");
   return NextResponse.json({ a: "1", b: 2, c: [1, 2, 3, 4], d: password });
 }
