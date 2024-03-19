baa911e745c019bc8f4c1f7ec767cfd7ae1cdd76
inajung <ina.jung@coint.co.kr>
2024-03-19 19:31:48 +0900

    feat: boolean 이 아닌 counter 로 재설계

--- src/store/isLock.ts
+++ src/store/isLock.ts
@@ -1,11 +1,15 @@
 import { create } from "zustand";
 
 interface IsLockState {
-  isLock: boolean;
-  setIsLock: (value: boolean) => void;
+  counter: number;
+  isLock: () => boolean;
+  increase: () => Promise<void>;
+  decrease: () => Promise<void>;
 }
 
-export const isLockState = create<IsLockState>((set) => ({
-  isLock: false,
-  setIsLock: (value) => set(() => ({ isLock: value })),
+export const isLockState = create<IsLockState>((set, get) => ({
+  counter: 0,
+  isLock: () => get().counter > 0,
+  increase: async () => set((state) => ({ counter: state.counter + 1 })),
+  decrease: async () => set((state) => ({ counter: state.counter + -1 })),
 }));
