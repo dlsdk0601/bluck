0bc4365bcae66b810e272e771f8d26471b080067
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-22 00:54:59 +0900

    feat: new/asset API try ~ catch 추가

--- src/app/api/asset/new/route.ts
+++ src/app/api/asset/new/route.ts
@@ -1,24 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { isNil } from "lodash";
 import { awsModel } from "@/lib/aws";
-import { ApiRes, badRequestException, internalServerException } from "@/lib/errorEx";
+import { ApiRes, badRequestException, ERR, internalServerException } from "@/lib/errorEx";
 import { isBlank } from "@/ex/utils";
 import { AssetNewReq, AssetNewRes } from "@/type/definitions";
 
 export async function POST(req: NextRequest): Promise<ApiRes<AssetNewRes>> {
-  const body: AssetNewReq = await req.json();
+  try {
+    const body: AssetNewReq = await req.json();
 
-  if (isBlank(body.base64) || isBlank(body.name)) {
-    return badRequestException("파일을 다시 업로드해주세요.");
+    if (isBlank(body.base64) || isBlank(body.name)) {
+      return badRequestException("파일을 다시 업로드해주세요.");
+    }
+
+    const asset = await awsModel.getFileSet(body.name, body.base64);
+
+    if (isNil(asset)) {
+      return internalServerException("파일 업로드에 실패하였습니다.");
+    }
+
+    return NextResponse.json({
+      fileSet: asset,
+    });
+  } catch (e: unknown) {
+    console.error(e);
+
+    let message = ERR.INTERNAL_SERVER;
+    if (e instanceof Error) {
+      message = e.message;
+    }
+    return NextResponse.json({
+      message,
+    });
   }
-
-  const asset = await awsModel.getFileSet(body.name, body.base64);
-
-  if (isNil(asset)) {
-    return internalServerException("파일 업로드에 실패하였습니다.");
-  }
-
-  return NextResponse.json({
-    fileSet: asset,
-  });
 }
