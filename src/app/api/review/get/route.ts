851d65dcaeefae9cb42878037fc9848743fba780
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-22 00:11:59 +0900

    feat: 불필요한 모듈 분리 제거

--- src/app/api/review/get/route.ts
+++ src/app/api/review/get/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { isNil } from "lodash";
 import { ApiRes, ERR, unAuthorizedException } from "@/lib/errorEx";
-import { ReviewBlog, ReviewGetReq, ReviewGetRes } from "@/type/definitions";
+import { ReviewGetReq, ReviewGetRes } from "@/type/definitions";
 import { auth } from "@/server/auth/auth";
 import prisma from "@/lib/prisma";
 import { awsModel } from "@/lib/aws";
@@ -15,12 +15,6 @@ export async function POST(req: NextRequest): Promise<ApiRes<ReviewGetRes>> {
 
   const body: ReviewGetReq = await req.json();
 
-  return NextResponse.json({
-    reviews: await getReviews(body.pk),
-  });
-}
-
-export async function getReviews(pk: number): Promise<ReviewBlog[]> {
   const reviews = await prisma.blog_review.findMany({
     select: {
       pk: true,
@@ -35,7 +29,7 @@ export async function getReviews(pk: number): Promise<ReviewBlog[]> {
       },
     },
     where: {
-      blog_pk: pk,
+      blog_pk: body.pk,
       deleted_at: null,
     },
     orderBy: {
@@ -43,14 +37,16 @@ export async function getReviews(pk: number): Promise<ReviewBlog[]> {
     },
   });
 
-  return reviews.map((rv) => ({
-    pk: rv.pk,
-    review: rv.review,
-    createAt: rv.created_at,
-    user: {
-      pk: rv.user.pk,
-      name: rv.user.name,
-      mainImage: awsModel.toFileSet(rv.user.main_image),
-    },
-  }));
+  return NextResponse.json({
+    reviews: reviews.map((rv) => ({
+      pk: rv.pk,
+      review: rv.review,
+      createAt: rv.created_at,
+      user: {
+        pk: rv.user.pk,
+        name: rv.user.name,
+        mainImage: awsModel.toFileSet(rv.user.main_image),
+      },
+    })),
+  });
 }
