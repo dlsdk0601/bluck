2a99001a0006c2795aab2c918c7daa4647514764
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-25 01:37:09 +0900

    feat: 불필요한 이미지 제거 및 아이콘, favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,27 @@ export const metadata: Metadata = {
   },
   description: "기술 블로그 BLCUK",
   metadataBase: new URL(config.baseUrl),
+  icons: {
+    icon: [
+      { url: "/icon/android-icon-192x192.png", sizes: "192x192", rel: "icon" },
+      { url: "/icon/android-icon-144x144.png", sizes: "144x144", rel: "icon" },
+      { url: "/icon/android-icon-96x96.png", sizes: "96x96", rel: "icon" },
+      { url: "/icon/favicon-72x72.png", sizes: "72x72", rel: "icon" },
+      { url: "/icon/favicon-48x48.png", sizes: "48x48", rel: "icon" },
+      { url: "/icon/android-icon-36x36.png", sizes: "36x36", rel: "icon" },
+    ],
+    apple: [
+      { url: "/icon/apple-icon-57x57.png", sizes: "57x57", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-60x60.png", sizes: "60x60", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-72x72.png", sizes: "72x72", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-76x76.png", sizes: "76x76", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-114x114.png", sizes: "114x114", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-120x120.png", sizes: "120x120", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-144x144.png", sizes: "144x144", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-152x152.png", sizes: "152x152", rel: "apple-touch-icon" },
+      { url: "/icon/apple-icon-180x180.png", sizes: "180x180", rel: "apple-touch-icon" },
+    ],
+  },
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
