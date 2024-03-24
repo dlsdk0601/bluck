9f31b81331fc870ff10fd50b6f2e759d3addd2d8
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-24 23:05:31 +0900

    fix: 댓글을 비로그인 유저도 볼수 있게 수정

--- src/app/api/review/get/route.ts
+++ src/app/api/review/get/route.ts
@@ -1,18 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { isNil } from "lodash";
-import { ApiRes, ERR, unAuthorizedException } from "@/lib/errorEx";
+import { ApiRes } from "@/lib/errorEx";
 import { ReviewGetReq, ReviewGetRes } from "@/type/definitions";
-import { auth } from "@/server/auth/auth";
 import prisma from "@/lib/prisma";
 import { awsModel } from "@/lib/aws";
 
 export async function POST(req: NextRequest): Promise<ApiRes<ReviewGetRes>> {
-  const session = await auth();
-
-  if (isNil(session?.user)) {
-    return unAuthorizedException(ERR.NOT_SIGN_USER);
-  }
-
   const body: ReviewGetReq = await req.json();
 
   const reviews = await prisma.blog_review.findMany({
